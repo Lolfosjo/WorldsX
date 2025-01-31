2c04c638445a46ab31578641d7ae51b62b7f2301
Blackjack200 <lx826444126@gmail.com>
2025-01-31 21:46:33 +0800

    ...

--- src/blackjack200/worldsx/command/subcommand/impl/InfoSubCommand.php
+++ src/blackjack200/worldsx/command/subcommand/impl/InfoSubCommand.php
@@ -31,7 +31,7 @@ class InfoSubCommand extends SubCommand {
 				$world->getSeed(),
 			]);
 			$this->mustSendTranslation($sender, 'command.info.generator', [
-				$world->getProvider()->getWorldData()->get()->getGenerator(),
+				$world->getProvider()->getWorldData()->getGenerator(),
 			]);
 			$this->mustSendTranslation($sender, 'command.info.time', [
 				$world->getTime(),
